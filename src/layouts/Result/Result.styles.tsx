defb875678dca3949047968098946ef2d778f21d
feliphepaz <feliphe.s.paz@gmail.com>
2023-06-06 08:42:08 -0300

    feat: result styles done

--- src/layouts/Result/Result.styles.tsx
+++ src/layouts/Result/Result.styles.tsx
@@ -1,48 +1,157 @@
 import styled from "styled-components";
 
 export const StyledResult = styled.section`
-  display: flex;
-  flex-direction: column;
-  gap: 100px;
-  .payment {
+  margin-top: 30px;
+
+  .head {
+    text-align: center;
+    margin-bottom: 20px;
+    line-height: 150%;
+
+    span {
+      font-size: 16px;
+      font-weight: 700;
+      color: #38b6ff;
+    }
+
     h2 {
-      font-weight: 500;
       font-size: 24px;
+      font-weight: 700;
+      background: linear-gradient(90deg, #38b6ff 0%, #02598c 53.93%);
+      -webkit-background-clip: text;
+      -webkit-text-fill-color: transparent;
+      background-clip: text;
+      margin: 5px 0px;
+    }
+
+    p {
+      font-size: 14px;
+      font-weight: 700;
+      color: rgba(0, 0, 0, 0.1);
+    }
+  }
+
+  > h2 {
+    font-weight: 700;
+    margin: 20px 0px;
+  }
+
+  .costs {
+    h2 {
+      font-weight: 700;
+      margin-bottom: 20px;
       line-height: 150%;
     }
 
-    .payments-flow {
-      margin: 40px 0px;
+    .costs-list {
+      display: flex;
+      gap: 30px;
+      overflow-x: auto;
+      padding-bottom: 40px;
 
-      li {
-        display: grid;
-        grid-template-columns: repeat(2, 1fr);
-        gap: 20px;
-        font-weight: 500;
-        font-size: 18px;
-        line-height: 150%;
-      }
+      .payment {
+        position: relative;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        min-width: 320px;
+        width: 320px;
+        height: 275px;
+        border: 4px solid #000000;
+        border-radius: 40px;
+        padding: 30px;
 
-      hr {
-        border: 0px;
-        height: 1px;
-        width: 100%;
-        background: #38b6ff;
-        margin: 15px 0px;
+        &.negative {
+          border: 4px solid #ff6242;
+
+          .total {
+            background: #ff6242;
+          }
+        }
+
+        &.positive {
+          border: 4px solid #16a100;
+
+          .total {
+            background: #16a100;
+          }
+        }
+
+        h3 {
+          font-weight: 700;
+          font-size: 24px;
+          line-height: 150%;
+          text-align: center;
+        }
+
+        .payments-flow {
+          margin: 20px 0px 24px 0px;
+          overflow-y: auto;
+          padding-right: 10px;
+
+          li {
+            display: flex;
+            flex-direction: column;
+            gap: 5px;
+
+            + li {
+              margin-top: 20px;
+            }
+
+            .payment-label {
+              font-weight: 700;
+              line-height: 150%;
+            }
+
+            .payment-value {
+              font-weight: 500;
+              color: #ff6242;
+              line-height: 150%;
+            }
+          }
+        }
+
+        .total {
+          display: flex;
+          position: absolute;
+          flex-direction: column;
+          width: max-content;
+          bottom: -30px;
+          line-height: 150%;
+          background: #000000;
+          padding: 6px 8px;
+          border-radius: 5px;
+          color: #ffffff;
+
+          .total-label {
+            font-weight: 700;
+          }
+
+          .total-value {
+            font-weight: 500;
+          }
+        }
       }
     }
+  }
 
-    .total {
+  .resume {
+    display: flex;
+    justify-content: center;
+    margin-top: 60px;
+
+    p {
+      line-height: 150%;
+      text-align: center;
+      max-width: 564px;
       font-weight: 500;
-      font-size: 20px;
-      line-height: 150%;
 
-      .positive {
-        color: green;
+      strong {
+        font-weight: 700;
       }
 
-      .negative {
-        color: red;
+      .invoicing {
+        color: #16a100;
       }
     }
   }
--- src/layouts/Result/index.tsx
+++ src/layouts/Result/index.tsx
@@ -15,51 +15,72 @@ export default function Result({ payments, totals }: ResultProps) {
 
   return (
     <StyledResult>
-      {payments.map((payment, paymentIndex) => (
-        <div key={paymentIndex} className="payment">
-          <h2>{paymentIndex + 1}º mês de vendas</h2>
-          <ul className="payments-flow">
-            {Array.from(
-              { length: paymentIndex + 1 },
-              (installment, installmentIndex) => (
-                <>
-                  <li key={installmentIndex}>
-                    <span className="payment-label">
-                      {installmentIndex + 1}º parcela do{" "}
-                      {paymentIndex + 1 - installmentIndex}º mês de vendas
-                    </span>
-                    <span className="payment-value">
-                      {formatter.format(payments[installmentIndex].liquid)}
-                    </span>
-                  </li>
-                  <hr />
-                </>
-              )
-            )}
-            <li>
-              <span className="payment-label">
-                Total custo equipe consórcio
-              </span>
-              <span className="payment-value">
-                {totals && formatter.format(totals.total)}
-              </span>
-            </li>
-          </ul>
-          <span className="total">
-            Déficit / Superavit:{" "}
-            <strong
+      <div className="head">
+        <span>Custo total</span>
+        <h2>R$ 37.420,00</h2>
+        <p>Caso não haja venda: R$ 27.852,00</p>
+      </div>
+      <div className="costs">
+        <h2>Demonstrativo de custos</h2>
+        <div className="costs-list">
+          {payments.map((payment, paymentIndex) => (
+            <div
+              key={paymentIndex}
               className={
                 totals && cumulativeValues[paymentIndex] - totals.total > 0
-                  ? "positive"
-                  : "negative"
+                  ? "positive payment"
+                  : "negative payment"
               }
             >
-              {totals &&
-                formatter.format(cumulativeValues[paymentIndex] - totals.total)}
-            </strong>
-          </span>
+              <h3>{paymentIndex + 1}º mês de vendas</h3>
+              <ul className="payments-flow">
+                {Array.from(
+                  { length: paymentIndex + 1 },
+                  (installment, installmentIndex) => (
+                    <>
+                      <li key={installmentIndex}>
+                        <span className="payment-label">
+                          {installmentIndex + 1}º parcela do{" "}
+                          {paymentIndex + 1 - installmentIndex}º mês de vendas
+                        </span>
+                        <span className="payment-value">
+                          {formatter.format(payments[installmentIndex].liquid)}
+                        </span>
+                      </li>
+                    </>
+                  )
+                )}
+                <li>
+                  <span className="payment-label">
+                    Total custo equipe consórcio
+                  </span>
+                  <span className="payment-value">
+                    {totals && formatter.format(totals.total)}
+                  </span>
+                </li>
+              </ul>
+              <div className="total">
+                <span className="total-label">Déficit / Superavit</span>
+                <span className="total-value">
+                  {totals &&
+                    formatter.format(
+                      cumulativeValues[paymentIndex] - totals.total
+                    )}
+                </span>
+              </div>
+            </div>
+          ))}
         </div>
-      ))}
+      </div>
+      <div className="resume">
+        <p>
+          Ao final de <strong>6 meses</strong> mantendo a média de{" "}
+          <strong>33 vendas</strong>, a concessionária terá formado uma carteira
+          de <strong>198 consorciados</strong> e pelo crédito médio de R$
+          35.000,00 terá R${" "}
+          <strong className="invoicing">6.930.000,00 à faturar</strong>.
+        </p>
+      </div>
     </StyledResult>
   );
 }
